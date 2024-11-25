94b685aa5fc7bed8a718186e3e170b6ba1a8da73
choidahey <daheychoe2000@gmail.com>
2024-11-26 00:25:31 +0900

    :sparkles: 스태미나 슬라이더 동적 처리

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,31 +1,16 @@
-using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     public static UIManager instance { get; private set; }
-    // Todo
-    // 설정에 마우스 감도 추가
-    private string Header = "[ UIManager ]";
-    private GameManager gameManager;
-
-    [Header("[Settings]")]
-    public GameObject setting_panel;
-
-    [Header("[Quit Setting]")]
-    public GameObject quit_panel;
-    public Button quit_yes_button;
-    public Button quit_no_button;
-    public Button quit_button;
-
-    private GameObject modal_background;
-    private Dictionary<Button, System.Action> buttonActions;
-
-    private bool isActivePanel = false;
 
     [Header("[Stamina]")]
     [SerializeField] private Slider staminaBar;
+
+    private Coroutine hideStaminaCoroutine;
+    private bool isStaminaBarVisible = false;
 
     private void Awake()
     {
@@ -35,118 +20,21 @@ public class UIManager : MonoBehaviour
             DontDestroyOnLoad(gameObject);
         }
         else
+        {
             Destroy(gameObject);
+        }
     }
-
 
     private void Start()
     {
-        InitializeGameManager();
-        CreateModalBackground();
-        RegisterQuitButtons();
-        OnControlQuitPanel(false);
-
         if (staminaBar != null)
-            staminaBar.value = 100f;
-        else
-            Debug.Log("스태미너 바 없음");
-    }
-
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape) && quit_panel.activeSelf)
         {
-            OnControlQuitPanel(false);
-        }
-        else if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            OnControlQuitPanel(true);
-        }
-    }
-
-
-    private void InitializeGameManager()
-    {
-        gameManager = FindObjectOfType<GameManager>();
-        if (gameManager == null)
-        {
-            Debug.Log(Header + " GameManager를 찾을 수 없습니다!");
-        }
-    }
-
-    private void CreateModalBackground()
-    {
-        // Canvas 아래에 모달 배경 생성
-        Canvas canvas = FindObjectOfType<Canvas>();
-        if (canvas == null)
-        {
-            Debug.LogError(Header + " Canvas를 찾을 수 없습니다!");
-            return;
-        }
-
-        // GameObject 생성
-        modal_background = new GameObject("ModalBackground");
-        modal_background.transform.SetParent(canvas.transform, false);
-
-        // Image 컴포넌트 추가
-        Image backgroundImage = modal_background.AddComponent<Image>();
-        backgroundImage.color = new Color(0, 0, 0, 0.9f); // 반투명 검은색 (투명도를 낮춤)
-
-        // RectTransform 설정
-        RectTransform rectTransform = modal_background.GetComponent<RectTransform>();
-        rectTransform.anchorMin = Vector2.zero; // 화면의 왼쪽 아래
-        rectTransform.anchorMax = Vector2.one;  // 화면의 오른쪽 위
-        rectTransform.offsetMin = Vector2.zero; // 패딩 없음
-        rectTransform.offsetMax = Vector2.zero;
-
-        // Raycast Block 설정 (UI 입력 차단)
-        backgroundImage.raycastTarget = true;
-
-        // ModalBackground를 Quit Panel 바로 뒤로 이동
-        if (quit_panel != null)
-        {
-            modal_background.transform.SetSiblingIndex(quit_panel.transform.GetSiblingIndex());
-        }
-
-        // 초기 상태 비활성화
-        modal_background.SetActive(false);
-    }
-
-    public void OnControlQuitPanel(bool flag)
-    {
-        if (quit_panel != null && modal_background != null)
-        {
-            quit_panel.SetActive(flag);
-            modal_background.SetActive(flag);
+            staminaBar.value = 1f;
+            staminaBar.gameObject.SetActive(false);
         }
         else
         {
-            Debug.LogError(Header + " Quit Panel 또는 Modal Background가 없습니다!");
-        }
-    }
-
-    private void RegisterQuitButtons()
-    {
-        // 버튼과 이벤트 매핑
-        buttonActions = new Dictionary<Button, System.Action>
-        {
-            { quit_yes_button, () => gameManager.Quit() },
-            { quit_no_button, () => OnControlQuitPanel(false) },
-            { quit_button, () => OnControlQuitPanel(true) }
-        };
-
-        // 버튼 이벤트 등록
-        foreach (var entry in buttonActions)
-        {
-            if (entry.Key != null)
-            {
-                entry.Key.onClick.RemoveAllListeners();
-                entry.Key.onClick.AddListener(() => entry.Value.Invoke());
-            }
-            else
-            {
-                Debug.LogError($"{Header} 버튼이 연결되지 않았습니다!");
-            }
+            Debug.LogError("스태미너 바 없음");
         }
     }
 
@@ -155,8 +43,48 @@ public class UIManager : MonoBehaviour
         if (staminaBar != null)
         {
             staminaBar.value = value;
+            ShowStaminaBar();
+
+            if (value >= 1f)
+            {
+                if (hideStaminaCoroutine != null)
+                {
+                    StopCoroutine(hideStaminaCoroutine);
+                }
+                hideStaminaCoroutine = StartCoroutine(HideStaminaBarWithDelay(1f));
+            }
+            else
+            {
+                if (hideStaminaCoroutine != null)
+                {
+                    StopCoroutine(hideStaminaCoroutine);
+                    hideStaminaCoroutine = null;
+                }
+            }
         }
         else
-            Debug.Log("스테미나 바 없음");
+        {
+            Debug.LogError("스태미나 바 없음");
+        }
+    }
+
+    private void ShowStaminaBar()
+    {
+        if (!isStaminaBarVisible && staminaBar != null)
+        {
+            staminaBar.gameObject.SetActive(true);
+            isStaminaBarVisible = true;
+        }
+    }
+
+    private IEnumerator HideStaminaBarWithDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        if (staminaBar != null)
+        {
+            staminaBar.gameObject.SetActive(false);
+            isStaminaBarVisible = false;
+        }
     }
 }
