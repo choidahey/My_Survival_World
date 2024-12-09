8290a801d69dd988cddeb836bbe0af19d16a9b88
choidahey <daheychoe2000@gmail.com>
2024-12-10 00:52:00 +0900

    :recycle: Jump 로직 및 애니메이터 수정

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -1,47 +1,62 @@
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public static PlayerController instance;
 
-    private float walkSpeed = 5f;
-    private float runScale = 5f;
-    private float jumpForce = 7f;
+    [SerializeField] private float walkSpeed = 5f;
+    [SerializeField] private float runScale = 5f;
+    [SerializeField] private float jumpForce = 7f;
+    [SerializeField] private float groundCheckDistance = 0.2f;
 
-    private bool isGrounded = true;
+    private bool isGround = true;
     private bool isFalling = false;
 
     private float currentH = 0f;
     private float currentV = 0f;
-
     private Vector3 currentDirection = Vector3.zero;
-    private LayerMask groundLayer;
 
     private Animator animator;
     private Rigidbody rigidBody;
-    private List<Collider> collisions = new List<Collider>();
+    private LayerMask groundMask;
 
     private float runStaminaCost = 30f;
 
-
-    private void Awake()
+    void Awake()
     {
-        if (GetComponent<Rigidbody>() != null)
-            rigidBody = GetComponent<Rigidbody>();
-
-        if (GetComponent<Animator>() != null)
-            animator = GetComponent<Animator>();
-
-        groundLayer = LayerMask.NameToLayer("Ground");
+        rigidBody = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+        groundMask = LayerMask.GetMask("Ground");
     }
 
-    private void Update()
+    void Update()
     {
+        CheckGrounded();
+
         Move();
         Jump();
+        UpdateFallingState();
 
+        animator.SetBool("isGround", isGround);
+        animator.SetBool("isFalling", isFalling);
+    }
+
+    private void CheckGrounded()
+    {
+        Ray ray = new Ray(transform.position + Vector3.up * 0.1f, Vector3.down);
+        if (Physics.Raycast(ray, groundCheckDistance, groundMask))
+        {
+            if (!isGround)
+            {
+                isFalling = false;
+            }
+            isGround = true;
+        }
+        else
+        {
+            isGround = false;
+        }
     }
 
     private void Move()
@@ -50,20 +65,22 @@ public class PlayerController : MonoBehaviour
         float h = Input.GetAxis("Horizontal");
 
         Transform camera = Camera.main.transform;
-
         bool isRunning = false;
 
         if (StaminaManager.instance.isExhausted)
         {
-            v *= 0f;
-            h *= 0f;
-
+            v = 0f;
+            h = 0f;
             animator.SetFloat("MoveSpeed", 0f);
-            // TODO :: 탈진 모션 추가하기
-
+            animator.SetBool("isExhausted", true);
             return;
         }
+        else
+        {
+            animator.SetBool("isExhausted", false);
+        }
 
+        // 달리기 조건
         if (Input.GetKey(KeyCode.LeftShift) && StaminaManager.instance.HasEnoughStamina(runStaminaCost * Time.deltaTime))
         {
             isRunning = true;
@@ -71,33 +88,21 @@ public class PlayerController : MonoBehaviour
             v *= runScale;
             h *= runScale;
         }
-        else
-        {
-            v *= 1f;
-            h *= 1f;
-            isRunning = false;
-        }
-
 
         float interpolation = 10f;
-
         currentV = Mathf.Lerp(currentV, v, Time.deltaTime * interpolation);
         currentH = Mathf.Lerp(currentH, h, Time.deltaTime * interpolation);
 
         Vector3 direction = camera.forward * currentV + camera.right * currentH;
-
         float directionLength = direction.magnitude;
-
-        direction.y = 0;
+        direction.y = 0f;
         direction = direction.normalized * directionLength;
 
         if (direction != Vector3.zero)
         {
             currentDirection = Vector3.Slerp(currentDirection, direction, Time.deltaTime * interpolation);
-
             transform.rotation = Quaternion.LookRotation(currentDirection);
             transform.position += currentDirection * walkSpeed * Time.deltaTime;
-
             animator.SetFloat("MoveSpeed", direction.magnitude);
         }
         else
@@ -117,19 +122,24 @@ public class PlayerController : MonoBehaviour
 
     private void Jump()
     {
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        if (Input.GetButtonDown("Jump") && isGround)
         {
-            isGrounded = false;
-            isFalling = true;
+            isFalling = false;
 
             Vector3 jumpVelocity = Vector3.up * Mathf.Sqrt(jumpForce * -Physics.gravity.y);
             rigidBody.AddForce(jumpVelocity, ForceMode.Impulse);
-
-            animator.SetBool("isGrounded", isGrounded);
-            animator.SetBool("isFalling", isFalling);
         }
+    }
 
-        animator.SetBool("isGrounded", isGrounded);
-        animator.SetBool("isFalling", isFalling);
+    private void UpdateFallingState()
+    {
+        if (!isGround && rigidBody.velocity.y < 0)
+        {
+            isFalling = true;
+        }
+        else if (isGround)
+        {
+            isFalling = false;
+        }
     }
 }
